520282564040150bbe5195e353d6dcd4bbe2784f
TouyamaHiroki <h-touyama@h-technology.jp>
2023-07-01 06:28:04 +0900

    items CRUD

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Faker\Guesser\Name;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -15,7 +17,9 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Items/Index', [
+            'items' => Item::select('id', 'name', 'price', 'is_selling')->get()
+        ]);
     }
 
     /**
@@ -25,7 +29,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +40,16 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index')->with([
+            'message' => '登録しました',
+            'status' => 'success'
+        ]);
     }
 
     /**
@@ -47,9 +60,11 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+        return Inertia::render('Items/Show', [
+            'item' => $item
+        ]);
     }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -58,7 +73,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render('Items/Edit', [
+            'item' => $item
+        ]);
     }
 
     /**
@@ -70,9 +87,19 @@ class ItemController extends Controller
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
-    }
+        $item->name = $request->name;
+        $item->memo = $request->memo;
+        $item->price = $request->price;
+        $item->is_selling = $request->is_selling;
+        $item->save();
 
+        return to_route('items.index')
+        ->with([
+            'message' => '更新しました',
+            'status' => 'success'
+        ]);
+    }
+    
     /**
      * Remove the specified resource from storage.
      *
@@ -81,6 +108,13 @@ class ItemController extends Controller
      */
     public function destroy(Item $item)
     {
-        //
+        $item->delete();
+        
+        return to_route('items.index')
+        ->with([
+            'message' => '削除しました',
+            'status' => 'danger'
+        ]);
+
     }
 }
